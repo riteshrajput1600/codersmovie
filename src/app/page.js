e5fb538762d6f624088f0a5c8409d94077c8db7e
Ritesh <riteshsingh1600@gmail.com>
2024-01-28 23:38:01 +0530

    Popular Setion created

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import Popular from "@/components/Popular";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="p-0 m-0">
       <Hero />
+      <Popular />
     </main>
   );
 }
